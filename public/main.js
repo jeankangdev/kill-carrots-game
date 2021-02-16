818c7217d9877a475b518ebb662fbbe062b4f875
Jean Kang <jeankang.dev@gmail.com>
2021-02-15 21:47:39 -0500

    relocate carrots and bugs

--- public/main.js
+++ public/main.js
@@ -1,16 +1,20 @@
+'use strict';
+
+const CARROT_SIZE = 80;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
 
 function initGame() {
   addItem('carrot', 5, 'img/carrot.png');
-  addItem('bug', 5, 'img/carrot.png');
+  addItem('bug', 5, 'img/bug.png');
 }
 
 function addItem(className, count, imgPath) {
   const x1 = 0;
   const y1 = 0;
-  const x2 = fieldRect.width;
-  const y2 = fieldRect.height;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
 
   for (let i = 0; i < count; i++) {
     const item = document.createElement('img');
